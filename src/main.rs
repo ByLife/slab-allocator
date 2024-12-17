c8fbfee81bc2dfb1b486dab91133adefc1f99fb1
bylife <bylife@bylife.fr>
2024-12-17 14:16:46 +0100

    Test allocateur

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
-}
+    let tab = [1, 2, 3, 4, 5];
+    
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    vec.push(3);
+    
+    let stro = String::from("salut");
+    
+    let boite = Box::new(42);
+    
+    let mut str_test = String::with_capacity(20);
+    str_test.push_str("test");
+    
+    println!("mon tab: {:?}", tab);
+    println!("mon vec: {:?}", vec);
+    println!("ma string lol: {}", stro);
+    println!("ma boite chelou: {}", boite);
+    println!("str de test: {} (capa: {})", str_test, str_test.capacity());
+ }
