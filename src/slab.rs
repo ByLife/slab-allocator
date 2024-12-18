68c0d6ae7af0921ac54fdd3955597973f208960d
bylife <bylife@bylife.fr>
2024-12-18 19:58:15 +0100

    Bug fixed, il fallait changer les chunks par pool

--- src/slab.rs
+++ src/slab.rs
@@ -3,6 +3,7 @@ use crate::{chunk::ChunkMetadata, error::AllocError};
 
 const ALIGNMENTS: [usize; 3] = [8, 16, 32];
 const SIZES: [usize; 3] = [32, 64, 128];
+const CHUNKS_PER_POOL: usize = 21; 
 
 pub struct SlabAllocator {
     memory: [u8; 4096],
@@ -14,13 +15,11 @@ impl SlabAllocator {
     pub const fn new() -> Self {
         const INIT_CHUNK: ChunkMetadata = ChunkMetadata::new(0, 0);
         
-        let mut allocator = SlabAllocator {
+        SlabAllocator {
             memory: [0; 4096],
             chunks: [INIT_CHUNK; 64],
             free_counts: [0; 3],
-        };
-        
-        allocator
+        }
     }
 
     pub fn init(&mut self) {
@@ -28,13 +27,15 @@ impl SlabAllocator {
         let mut chunk_idx = 0;
         
         for (pool_idx, &size) in SIZES.iter().enumerate() {
-            let chunks_in_pool = 4096 / (3 * size);
-            self.free_counts[pool_idx] = chunks_in_pool;
+            let pool_offset = pool_idx * CHUNKS_PER_POOL;
+            self.free_counts[pool_idx] = CHUNKS_PER_POOL;
             
-            for _ in 0..chunks_in_pool {
-                self.chunks[chunk_idx] = ChunkMetadata::new(offset, size);
-                offset += size;
-                chunk_idx += 1;
+            for i in 0..CHUNKS_PER_POOL {
+                if chunk_idx < self.chunks.len() {
+                    self.chunks[chunk_idx] = ChunkMetadata::new(offset, size);
+                    offset += size;
+                    chunk_idx += 1;
+                }
             }
         }
     }
@@ -51,8 +52,8 @@ impl SlabAllocator {
             return Err(AllocError::InvalidAlign);
         }
         
-        let start_idx = pool_idx * (64 / SIZES.len());
-        let end_idx = start_idx + (64 / SIZES.len());
+        let start_idx = pool_idx * CHUNKS_PER_POOL;
+        let end_idx = start_idx + CHUNKS_PER_POOL;
         
         for i in start_idx..end_idx {
             if !self.chunks[i].is_used {
